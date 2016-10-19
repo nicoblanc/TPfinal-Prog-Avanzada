71be780dadd0fe5e895fa0e2e553aca95d6e54dc
mauribesson <mauribesson@gmail.com>
2016-10-18 23:44:05 -0300

    login

--- code/application/views/base/head_view.php
+++ code/application/views/base/head_view.php
@@ -74,6 +74,9 @@
                         <li><a href="<?php echo base_url('/index.php/user/show_crud_view'); ?>"><i class="fa fa-plus"></i> Usuarios</a></li>
 
                         <li><a href="<?php echo base_url('/index.php/client/show_crud_view'); ?>"><i class="fa fa-plus"></i> Cliente</a></li>
+
+                        <li><a href="<?php echo base_url('/index.php/verificar'); ?>"><i class="fa fa-user"></i> Login</a></li>
+
                     </ul>
 
                     <!--
